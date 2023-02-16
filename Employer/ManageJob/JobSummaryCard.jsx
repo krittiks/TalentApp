d4ead042cec423464dd0eea1ef1a50067294c5ae
Krittika <114709329+krittiks@users.noreply.github.com>
2023-02-16 14:22:37 +1300

    Add files via upload

---
+++ Employer/ManageJob/JobSummaryCard.jsx
@@ -0,0 +1,52 @@
+﻿import React from 'react';
+import Cookies from 'js-cookie';
+import { Popup } from 'semantic-ui-react';
+import moment from 'moment';
+
+export class JobSummaryCard extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('props.location.state.params.id',props.location.state.params.id)
+
+        this.state={
+            jid : this.props.location.state.params.id
+        }
+
+        this.selectJob = this.selectJob.bind(this)
+    }
+
+    selectJob(id) {
+        var cookies = Cookies.get('talentAuthToken');
+        $.ajax({
+          //  url:  'http://localhost:51689/listing/listing/closeJob',
+          url : 'https://webapptalent.azurewebsites.net/listing/listing/closeJob',
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            dataType:'json',
+            type: "post",
+            data: JSON.stringify(id),
+            success: function (res) {
+                if (res.success == true) {
+                    TalentUtil.notification.show(res.message, "success", null, null);
+                    window.location = "/ManageJobs";
+                   
+                } else {
+                    TalentUtil.notification.show(res.message, "error", null, null)
+                }
+                
+            }.bind(this)
+        })
+
+    }
+
+    render() {
+        
+        return(
+        
+        this.selectJob(this.state.jid)
+        
+        )
+    }
+}
---
+++ Employer/ManageJob/ManageJob.jsx
@@ -0,0 +1,177 @@
+﻿import React, { Fragment } from 'react';
+import Cookies from 'js-cookie';
+import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
+import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
+import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
+import { Dropdown, Pagination, Table,Icon } from 'semantic-ui-react';
+import JobList from './JobList.jsx';
+
+export default class ManageJob extends React.Component {
+    constructor(props) {
+        super(props);
+        let loader = loaderData
+        loader.allowedUsers.push("Employer");
+        loader.allowedUsers.push("Recruiter");
+        console.log('loader',loader)
+        this.state = {
+            loadJobs: [],
+            loaderData: loader,
+            activePage: 1,
+            postPerPage:3,            
+            sortBy: {
+                date: "desc"
+            },
+            filter: {
+                showActive: true,
+                showClosed: false,
+                showDraft: true,
+                showExpired: true,
+                showUnexpired: true
+            },
+            totalPages: 1,
+            activeIndex: "",
+            
+        }
+        this.loadData = this.loadData.bind(this);
+        this.init = this.init.bind(this);
+        this.loadNewData = this.loadNewData.bind(this);
+       
+        //your functions go here
+      
+    };
+
+    init() {
+        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        loaderData.isLoading = false;
+        
+        //this.setState({ loaderData, });//comment this
+
+       // set loaderData.isLoading to false after getting data
+         this.loadData(() =>
+            this.setState({ loaderData, })
+         )
+
+    }
+
+    componentDidMount() {
+        this.init();        
+        this.loadData();
+      
+    };    
+
+    loadData(callback) {
+      // var link='http://localhost:51689/listing/listing/getSortedEmployerJobs';
+      var link ='https://webapptalent.azurewebsites.net/listing/listing/getSortedEmployerJobs';
+        var cookies = Cookies.get('talentAuthToken');
+       // your ajax call and other logic goes here
+       $.ajax({
+        url:link,
+        headers: {
+            'Authorization': 'Bearer ' + cookies,
+            'Content-Type': 'application/json'
+        },
+        data:{
+            activePage:this.state.activePage,
+            showActive:this.state.filter.showActive,
+            showClosed:this.state.filter.showClosed,
+            showExpired:this.state.filter.showExpired,
+            showUnexpired:this.state.filter.showUnexpired
+        },
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (res) {
+            //let loadJobs = [];
+            if (res.myJobs) {
+                this.setState( {loadJobs : res.myJobs })
+            }
+            loaderData.isLoading = false;
+           
+        }.bind(this),
+        error: function (res) {
+            console.log(res.status)
+        }
+    }) 
+    }
+
+    loadNewData(data) {
+        var loader = this.state.loaderData;
+        loader.isLoading = true;
+        data[loaderData] = loader;
+        this.setState(data, () => {
+            this.loadData(() => {
+                loader.isLoading = false;
+                this.setState({
+                    loadData: loader
+                })
+            })
+        });
+    }
+
+   
+    render() {        
+        var indexOfLastPost = this.state.activePage * this.state.postPerPage
+        var indexOfFirstPost =  indexOfLastPost - this.state.postPerPage   
+
+        return (
+        <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+               <div className ="ui container">
+               <h2>List of Jobs</h2>
+                <Table collapsing basic="very" >
+                <Table.Header>
+                    
+                <Table.Row>
+                    <Table.Cell > <Icon name='filter'/>Filter:                     
+                    <Dropdown 
+                    text ='Choose filter'/> 
+                     &nbsp;&nbsp;&nbsp; 
+                    <Icon name='calendar alternate'/>Sort by date:                    
+                    <Dropdown text='Newest first' >
+                        
+                    </Dropdown>                
+                    </Table.Cell>
+
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                       <Table.Row>
+                        <Table.Cell>
+                        <JobList
+                        loadJobs={this.state.loadJobs}
+                        indexOfFirstPost={indexOfFirstPost}
+                        indexOfLastPost={indexOfLastPost}
+                        />
+                        </Table.Cell>
+                        </Table.Row>
+                </Table.Body>
+            
+                </Table>
+                
+                <div style={{display : 'inline-block', 
+                                marginTop : 30,
+                                marginLeft : '45%'
+                            }}>
+
+               
+                 <Pagination
+                    defaultActivePage={this.state.activePage}
+                    firstItem={{ content: <Icon name='angle double left' />, icon: true }}
+                    lastItem={{ content: <Icon name='angle double right' />, icon: true }}
+                    prevItem={{ content: <Icon name='angle left' />, icon: true }}
+                    nextItem={{ content: <Icon name='angle right' />, icon: true }}
+                    totalPages={Math.ceil(this.state.loadJobs.length/this.state.postPerPage)}
+                    boundaryRange={0}
+                    ellipsisItem={null}
+                    siblingRange={0}
+                    onPageChange={(e, { activePage }) => this.setState({ activePage }) }
+                 />
+              
+              
+            </div>
+            </div>
+                <br/><br/>
+        </BodyWrapper>  
+        )
+    }
+}
